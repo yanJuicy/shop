e08a0bc702efeafadde6cff302d9f7342c8c3d28
Yanjoo <hanjub3@gmail.com>
2021-12-03 13:56:13 +0900

    Spring DATA JPA @Query 어노테이션

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("SELECT i FROM Item i WHERE i.itemDetail LIKE %:itemDetail% ORDER BY i.price DESC")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="SELECT * FROM item i WHERE i.item_detail LIKE %:itemDetail% ORDER BY i.price DESC", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }

a20c12c9c38613e3c926750698f2921eb650ed24
Yanjoo <hanjub3@gmail.com>
2022-01-12 21:07:54 +0900

    다대일 단방향 매핑

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,35 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+
+}
